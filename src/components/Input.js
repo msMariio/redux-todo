8afa2a8d2626385babfd0bc860276ee3a931f852
msMariio <55538435+msMariio@users.noreply.github.com>
2021-02-04 18:04:37 +0100

    Componente input

---
+++ src/components/Input.js
@@ -0,0 +1,20 @@
+import React,{ useState } from 'react'
+import './Input.css' 
+
+const Input = () => {
+
+    const [input, setInput] = useState ('')
+
+    const addTodo = () => {
+
+    }
+
+    return (
+        <div className='input'>
+            <input type="text" value={input} onChange={e=>setInput(e.target.value)}/>
+            <button onClick={addTodo}>Añadir</button>
+        </div>
+    )
+}
+
+export default Input
